a57de3d976a913f2798ad4d0a1442e0ef87fbbac
henrymxu <henry.m.xu@live.com>
2020-04-12 01:40:27 -0400

    Refactor test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,84 +3,88 @@ import unittest
 from tft import window, image_utils, game, parser, board
 
 TestWindowName = "TFTAnalyzer Test Window"
+Test1080PDefault = "/Users/henry/Downloads/TFT Screenshots/Screenshot_10.png"
+Test1440PDefault = "/Users/henry/Downloads/TFT Screenshots/Screenshot_11.png"
 
 
-class TestParserPlayers(unittest.TestCase):
+class TestParser(unittest.TestCase):
+
     def test_players_1080p(self):
-        self.test_players("/Users/henry/Downloads/TFT Screenshots/Screenshot_12.png")
-
-    def test_players_1440p(self):
-        self.test_players("/Users/henry/Downloads/TFT Screenshots/Screenshot_13.png")
-
-    def test_players(self, file):
-        gameWindow, gameBoard = initialize_screenshot(file)
+        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_12.png")
         players = game.retrieve_player_list(gameWindow, gameBoard)
         self.assertEqual(len(players), 8)
         for player in players:
             self.assertNotEqual("", player)
 
+    def test_players_1440p(self):
+        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_13.png")
+        players = game.retrieve_player_list(gameWindow, gameBoard)
+        self.assertEqual(len(players), 8)
+        for player in players:
+            self.assertNotEqual("", player)
 
-class TestParserHealthbars(unittest.TestCase):
-    def test_healthbars(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_11.png")
+    def test_healthbars_1080p(self):
+        gameWindow, gameBoard = initialize_screenshot(
+            Test1080PDefault)
         img = gameWindow.captureWindow()
         top_to_bottom = board.crop_healthbar(img, gameBoard, 0)
         bottom_to_top = board.crop_healthbar(img, gameBoard, 1)
         healthbars = parser.parse_healthbars(top_to_bottom, bottom_to_top)
         print(healthbars)
 
+    def test_healthbars_1440p(self):
+        gameWindow, gameBoard = initialize_screenshot(
+            Test1440PDefault)
+        img = gameWindow.captureWindow()
+        top_to_bottom = board.crop_healthbar(img, gameBoard, 0)
+        bottom_to_top = board.crop_healthbar(img, gameBoard, 1)
+        healthbars = parser.parse_healthbars(top_to_bottom, bottom_to_top)
+        print(healthbars)
 
-class TestParserLevel(unittest.TestCase):
     def test_level_1080p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_10.png")
+        gameWindow, gameBoard = initialize_screenshot(Test1080PDefault)
         img = gameWindow.captureWindow()
         level = parser.parse_level(board.crop_level(img, gameBoard))
         self.assertEqual(level, "6")
 
     def test_level_1440p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_11.png")
+        gameWindow, gameBoard = initialize_screenshot(Test1440PDefault)
         img = gameWindow.captureWindow()
         level = parser.parse_level(board.crop_level(img, gameBoard))
         self.assertEqual(level, "8")
 
-
-class TestParserStage(unittest.TestCase):
-    def test_level_1080p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_10.png")
+    def test_stage_1080p(self):
+        gameWindow, gameBoard = initialize_screenshot(Test1080PDefault)
         img = gameWindow.captureWindow()
         stage = parser.parse_stage(board.crop_stage(img, gameBoard))
         self.assertEqual(stage, "4-5")
 
-    def test_level_1440p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_11.png")
+    def test_stage_1440p(self):
+        gameWindow, gameBoard = initialize_screenshot(Test1440PDefault)
         img = gameWindow.captureWindow()
         stage = parser.parse_stage(board.crop_stage(img, gameBoard))
         self.assertEqual(stage, "5-5")
 
-
-class TestParserShop(unittest.TestCase):
     def test_level_1080p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_10.png")
+        gameWindow, gameBoard = initialize_screenshot(Test1080PDefault)
         img = gameWindow.captureWindow()
         shop = parser.parse_shop(board.crop_shop(img, gameBoard))
         self.assertEqual(shop, ['Blitzerank', 'Graves', 'Ziggs', 'Zoe', 'Vi'])
 
     def test_level_1440p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_11.png")
+        gameWindow, gameBoard = initialize_screenshot(Test1440PDefault)
         img = gameWindow.captureWindow()
         shop = parser.parse_shop(board.crop_shop(img, gameBoard))
         self.assertEqual(shop, ['Rakan', '', 'Yasuo', 'Vel’iKoz', 'Sona'])
 
-
-class TestParserGold(unittest.TestCase):
     def test_level_1080p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_10.png")
+        gameWindow, gameBoard = initialize_screenshot(Test1080PDefault)
         img = gameWindow.captureWindow()
         gold = parser.parse_gold(board.crop_gold(img, gameBoard))
         self.assertEqual(gold, "50")
 
     def test_level_1440p(self):
-        gameWindow, gameBoard = initialize_screenshot("/Users/henry/Downloads/TFT Screenshots/Screenshot_11.png")
+        gameWindow, gameBoard = initialize_screenshot(Test1440PDefault)
         img = gameWindow.captureWindow()
         gold = parser.parse_gold(board.crop_gold(img, gameBoard))
         self.assertEqual(gold, "20")
